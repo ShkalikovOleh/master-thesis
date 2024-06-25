ac4550aace2a072f5a92ebbdcaf173b7812a41cd
Oleh Shkalikov <Shkalikov.Oleh@gmail.com>
2024-06-25 20:14:34 +0200

    Add pipeline runner

--- src/pipeline/run_pipeline.py
+++ src/pipeline/run_pipeline.py
@@ -1,2 +1,74 @@
 """This module gives ability to run a pipeline with specified congif from the
 configs directory"""
+
+import logging
+from collections import namedtuple
+from pathlib import Path
+
+import hydra
+import networkx as nx
+from omegaconf import DictConfig, OmegaConf
+
+
+from src.utils.pipeline import (
+    create_pipeline_graph,
+    instantiate_transforms,
+    shrink_cached,
+)
+
+Transform = namedtuple("Transform", ["name", "deps", "transform"])
+
+logger = logging.getLogger("Pipeline")
+
+
+def execute_pipeline(
+    transforms: list[Transform],
+    cached_step_paths: DictConfig,
+) -> None:
+    graph = create_pipeline_graph(transforms)
+    if not nx.is_directed_acyclic_graph(graph):
+        logger.critical("Pipeline has cycle(s)!")
+        raise ValueError("Pipeline graph is not a DAG! Please fix a config")
+
+    if cached_step_paths is not None:
+        logging.info("Shrink pipeline and reuse cached outputs")
+        cache = OmegaConf.to_container(cached_step_paths, resolve=True)
+        graph, transforms = shrink_cached(graph, transforms, cache)
+
+    step_outs = {}
+    for name in nx.topological_sort(graph):
+        step = transforms[graph.nodes[name]["idx"]]
+        logger.info(f"Start {name} pipeline step")
+
+        inputs = [step_outs[node] for node in step.deps]
+        out = step.transform(*inputs)
+        step_outs[name] = out
+
+
+@hydra.main(config_path="configs", config_name="default", version_base="1.3")
+def main(cfg: DictConfig) -> None:
+    logging.info("Instantiate pipeline and runner")
+    transforms = instantiate_transforms(cfg.pipeline)
+
+    if cfg["log_to_wandb"]:
+        import wandb
+
+        run = wandb.init(
+            project=cfg["wandb_project"], config=OmegaConf.to_container(cfg)
+        )
+        with run:
+            logger.info(f"Start wandb logging into run: {run.name} with id {run.id}")
+
+            execute_pipeline(
+                transforms,
+                cfg["cached_step_paths"],
+            )
+
+            run.log_artifact(Path.cwd() / ".hydra/config.yaml")
+            run.log_artifact(Path.cwd() / ".hydra/overrides.yaml")
+    else:
+        execute_pipeline(transforms, cfg["cached_step_paths"])
+
+
+if __name__ == "__main__":
+    main()
--- src/utils/pipeline.py
+++ src/utils/pipeline.py
@@ -1,1 +1,84 @@
 """Utilities for running pipelines"""
+
+from collections import namedtuple
+from typing import Any
+from omegaconf import DictConfig
+
+from hydra.utils import instantiate
+
+import networkx as nx
+
+from src.pipeline.data import LoadDataset
+
+
+Transform = namedtuple("Transform", ["name", "deps", "transform"])
+
+
+def instantiate_transforms(transforms_cfg: DictConfig) -> list[Transform]:
+    result = []
+    for name, transform in transforms_cfg.items():
+        trans = instantiate(transform["transform"])
+        result.append(Transform(name, transform["deps"], trans))
+    return result
+
+
+def shrink_cached(
+    graph: nx.DiGraph,
+    transforms: list[Transform],
+    cached_step_outs: dict[str, Any],
+) -> list[Transform]:
+    # Find all steps that already included in cache
+    cached_deps = set()
+    for node in cached_step_outs:
+        cached_deps.update(nx.ancestors(graph, node))
+
+    changed = True
+    while changed:
+        changed = False
+        non_cached = set()
+        for cand in cached_deps:
+            for edge in graph.edges(cand):
+                ancestor = edge[1]
+                # there is an edge to non cached step
+                if ancestor not in cached_deps and ancestor not in cached_step_outs:
+                    # can not remove step since there is a deps to non cached transform
+                    non_cached.add(cand)
+                    changed = True
+        for prev_node in non_cached:
+            cached_deps.remove(prev_node)
+
+    shrinked_transforms = []
+    idx = 0
+    for transform in transforms:
+        node = transform.name
+        if node in cached_step_outs:
+            pipe_step = LoadDataset(cached_step_outs[node])
+            new_transform = Transform(node, [], pipe_step)
+            shrinked_transforms.append(new_transform)
+            graph.nodes[node]["idx"] = idx
+            idx += 1
+        elif node not in cached_deps:
+            shrinked_transforms.append(transform)
+            graph.nodes[node]["idx"] = idx
+            idx += 1
+        else:
+            graph.remove_node(node)
+
+    return graph, shrinked_transforms
+
+
+def create_pipeline_graph(transforms: list[Transform]) -> nx.DiGraph:
+    graph = nx.DiGraph()
+
+    # add all nodes
+    for idx, transform in enumerate(transforms):
+        v = transform.name
+        graph.add_node(v, idx=idx)
+
+    # add edges
+    for transform in transforms:
+        v = transform.name
+        for u in transform.deps:
+            graph.add_edge(u, v)
+
+    return graph
